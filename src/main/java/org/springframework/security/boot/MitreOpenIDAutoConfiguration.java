ceaec325c93db0e76fe598d419fc0fed5d95cb61
wandl <hnxyhcwdl1003@163.com>
2019-10-21 17:41:32 +0800

    调整初始化顺序

--- src/main/java/org/springframework/security/boot/MitreOpenIDAutoConfiguration.java
+++ src/main/java/org/springframework/security/boot/MitreOpenIDAutoConfiguration.java
@@ -32,6 +32,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -39,6 +40,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.util.ObjectUtils;
@@ -210,6 +212,7 @@ public class MitreOpenIDAutoConfiguration implements ApplicationContextAware {
 	@Configuration
 	@ConditionalOnWebApplication
 	@ComponentScan("org.mitre.openid")
+	@Order(SecurityProperties.DEFAULT_FILTER_ORDER + 6)
 	public static class MitreOauth2Configuration {
 		
 	}
