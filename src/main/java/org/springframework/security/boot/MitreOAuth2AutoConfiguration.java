85d8169f68c7a1a4b943f123830bfbd401963f60
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 15:57:00 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/MitreOAuth2AutoConfiguration.java
+++ src/main/java/org/springframework/security/boot/MitreOAuth2AutoConfiguration.java
@@ -37,7 +37,7 @@ import org.springframework.util.ObjectUtils;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * https://github.com/mitreid-connect/OpenID-Connect-Java-Spring-Server/wiki/Token-Introspecting-Client-Config
  */
 @Configuration
